9b3375dabe9ecbd84f63c947b356f6cf031b73ac
aking1 <Aleksey@ak-mbp-14.local>
2018-04-05 22:27:39 +0300

    math syseq added

--- auto/math.el
+++ auto/math.el
@@ -1,7 +1,28 @@
 (TeX-add-style-hook
  "math"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "cppL")
+   (add-to-list 'LaTeX-verbatim-environments-local "textL")
+   (add-to-list 'LaTeX-verbatim-environments-local "pycode")
+   (add-to-list 'LaTeX-verbatim-environments-local "pyc")
    (TeX-run-style-hooks
-    "amsmath"))
+    "amsmath"
+    "amssymb"
+    "bm"
+    "breqn")
+   (TeX-add-symbols
+    '("sbra" 1)
+    '("rbra" 1)
+    '("xxevat" 1)
+    '("xevat" 1)
+    '("evat" 1)
+    "lft"
+    "rgt"
+    "xlft"
+    "xrgt")
+   (LaTeX-add-environments
+    "xcases"
+    "syseq"))
  :latex)
 
