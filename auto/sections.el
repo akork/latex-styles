92545aed408b68eb8f0e4da48edf15ed46e99b17
aking1 <Aleksey@ak-mbp-14.local>
2018-04-15 23:25:38 +0300

    deleted .el .log

--- auto/sections.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "sections"
- (lambda ()
-   (setq TeX-command-extra-options
-         "-recorder -synctex=1 -interaction=nonstopmode -shell-escape --src-specials")
-   (TeX-run-style-hooks
-    "titlesec"))
- :latex)
-
